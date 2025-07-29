63db8a451f94a50f087ff36a2ff15a29c7dafdb9
elneves81 <neves.elber@gmail.com>
2025-07-29 09:44:07 -0300

    Fix: Nova função salas mínima funcional

--- netlify/functions/salas.js
+++ netlify/functions/salas.js
@@ -1,1 +1,13 @@
-// Teste
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify([
+      {id: 1, nome: 'Sala A', capacidade: 10, disponivel: true},
+      {id: 2, nome: 'Sala B', capacidade: 6, disponivel: true}
+    ])
+  };
+};
